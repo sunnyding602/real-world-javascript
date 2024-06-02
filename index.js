e8e0541aa6d24554d2ea6216c6efc9e8c313476e
Runxi Ding <runxi@getepic.com>
2024-06-02 13:16:56 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import User from './models/user.js'
+import Tweet from './models/tweet.js'
+
+const app = express()
+const url = 'mongodb+srv://runxi:password@cluster0.stndwtf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+async function main() {
+    await mongoose.connect(url).then(() => console.log('Database Connected!'))
+
+    app.get('/users', async (req, res) => {
+        const allUsers = await User.find()
+        res.send(allUsers)
+    })
+
+    app.get('/users/:id', async (req, res) => {
+        const userId = req.params.id
+        const user = await User.findOne({ _id: userId })
+        res.send(user)
+    })
+    
+    app.listen(3000, () => console.log('Server listening on port 3000 : http://localhost:3000/users'))
+}
+
+main()
